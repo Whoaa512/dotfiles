ab5b88f66257b1e52c06e56626c8438b15e30dc0
C.J. Winslow <whoaa512@gmail.com>
2015-12-28 14:36:48 -0800

    remove the dry run from rsync

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ localSource="$(cd "$(dirname ".zshrc")"; pwd)/$(basename ".zshrc")"
 git pull origin master;
 
 function doIt() {
-	rsync -n --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude ".zshrc" --exclude "dotfiles.sublime-workspace" \
 		--exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
 	ln -s localSource ~/.zshrc
