b0ee14ef8131657360416b3e7267bf25a430d045
C.J. Winslow <whoaa512@gmail.com>
2018-07-21 20:29:32 -0700

    Add zsh-autosuggestions to brew apps

--- brew.sh
+++ brew.sh
@@ -32,6 +32,7 @@ brew install bash
 brew tap homebrew/versions
 brew install bash-completion2
 brew install zsh-syntax-highlighting
+brew install zsh-autosuggestions
 
 # Install `wget` with IRI support.
 brew install wget --with-iri
