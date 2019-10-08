1b6af7ad96b1d489eb594de035fe16218b45dc92
C.J. Winslow <whoaa512@gmail.com>
2019-10-07 21:54:58 -0700

    Add some zsh plugins

--- non-brew.sh
+++ non-brew.sh
@@ -3,6 +3,11 @@
 echo "Please ensure you have Sublime Text 3 installed. Adding symlink"
 ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ~/bin/subl
 
+echo "Please ensure you have Oh My Zsh installed"
+git clone https://github.com/adolfoabegg/browse-commit ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/browse-commit
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 # Node
 cd ~/code && git clone https://github.com/tj/n.git
 cd n && make install
