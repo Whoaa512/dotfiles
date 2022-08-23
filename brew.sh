e5edb3b694ffd62fa39f292e672faa352e25b068
CJ Winslow <whoaa512@gmail.com>
2022-08-22 19:43:59 -0700

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -12,7 +12,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 brew update
 
 # Upgrade any already-installed formulae.
-brew upgrade --all
+# brew upgrade
 
 # Install GNU core utilities (those that come with OS X are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
@@ -24,7 +24,7 @@ brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
+brew install gnu-sed #--with-default-names
 # Install Bash 4.
 # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
 # running `chsh`.
@@ -35,20 +35,20 @@ brew install zsh-syntax-highlighting
 brew install zsh-autosuggestions
 
 # Install `wget` with IRI support.
-brew install wget --with-iri
+brew install wget #--with-iri
 
 # Install RingoJS and Narwhal.
 # Note that the order in which these are installed is important;
 # see http://git.io/brew-narwhal-ringo.
-brew install ringojs
-brew install narwhal
+# brew install ringojs
+# brew install narwhal
 
 # Install more recent versions of some OS X tools.
-brew install vim --override-system-vi
+brew install vim #--override-system-vi
 brew install homebrew/dupes/grep
 brew install homebrew/dupes/openssh
 brew install homebrew/dupes/screen
-brew install homebrew/php/php55 --with-gmp
+# brew install homebrew/php/php55 --with-gmp
 
 # Install font tools.
 brew tap bramstein/webfonttools
@@ -86,6 +86,7 @@ brew install xpdf
 brew install xz
 
 # Install other useful binaries.
+brew install exa
 brew install lsd
 brew install ack
 brew install dark-mode
@@ -97,15 +98,16 @@ brew install git-lfs
 brew install htop
 brew install hub
 brew install jq
-brew install imagemagick --with-webp
+brew install imagemagick #--with-webp
 brew install lua
 brew install lynx
-brew install mas
+# brew install mas
+brew install mosh
 brew install p7zip
 brew install pigz
 brew install pv
 brew install rename
-brew install rhino
+# brew install rhino
 brew install s3cmd
 brew install spoof-mac
 brew install ssh-copy-id
@@ -116,43 +118,46 @@ brew install webkit2png
 brew install youtube-dl
 brew install zopfli
 brew install fzf
+brew install pygitup
 
 # Cask installs
 # prerequisites:
 brew tap caskroom/fonts
 brew tap caskroom/versions
 
-brew cask install alfred
-brew cask install atom
-brew cask install battle-net
-brew cask install bettertouchtool
-brew cask install disk-inventory-x
-brew cask install dropbox
-brew cask install firefox
-brew cask install flux
-brew cask install font-source-code-pro
-brew cask install google-chrome
-brew cask install google-drive
-brew cask install iterm2
-brew cask install minecraft
-brew cask install ngrok
-brew cask install omnifocus
-brew cask install skype
-brew cask install sourcetree
-brew cask install spotify
-brew cask install stay
-brew cask install vlc
+brew install --cask alfred
+# brew install --cask atom
+# brew install --cask battle-net
+brew install --cask bettertouchtool
+# brew install --cask disk-inventory-x
+# brew install --cask dropbox
+brew install --cask firefox
+# brew install --cask flux
+brew install homebrew/cask-fonts/font-source-code-pro
+brew install homebrew/cask-fonts/font-source-code-pro-for-powerline
+# brew install --cask google-chrome
+# brew install --cask google-drive
+brew install --cask iterm2
+# brew install --cask minecraft
+brew install --cask monitorcontrol
+# brew install --cask ngrok
+# brew install --cask omnifocus
+# brew install --cask skype
+brew install --cask sourcetree
+brew install --cask spotify
+# brew install --cask stay
+# brew install --cask vlc
 # Dash should be installed via AppStore, since I purchased v3 from there
-# brew cask install dash
+# brew install --cask dash
 
 # Mac Store installs
-mas sigin whoaa512@gmail.com
-mas install 803453959 # Slack
-mas install 449589707 # Dash
-mas install 866773894 # Quiver
+# mas sigin whoaa512@gmail.com
+# mas install 803453959 # Slack
+# mas install 449589707 # Dash
+# mas install 866773894 # Quiver
 
 # Quick look plugins https://github.com/sindresorhus/quick-look-plugins
-brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package
+# brew install --cask qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package
 
 # Remove outdated versions from the cellar.
 brew cleanup
