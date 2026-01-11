5b586252676e81ae2a86008b72a034fccf9ceeb3
CJ Winslow <github@cjwinslow.com>
2026-01-10 22:16:34 -0800

    chore: rsync exclude pattern cleanup

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ localSource="$(cd "$(dirname ".zshrc")"; pwd)/$(basename ".zshrc")"
 # git pull origin master;
 
 function doIt() {
-	rsync --exclude ".git/" \
+	rsync --exclude ".git" \
 		--exclude ".DS_Store" \
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
